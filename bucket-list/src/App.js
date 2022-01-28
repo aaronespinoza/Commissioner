bf5406a11590b002981ba267481e20351c47b6a6
Jay <jbarbanel531@gmail.com>
2022-01-28 14:30:10 -0800

    starting front end

---
+++ bucket-list/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Navbar} from "react-bootstrap"
+import img from './images/test.png'
+
+function App() {
+  return (
+    <div>
+   <Card styles={{height:'300px'}}className="bg-dark text-white">
+  <Card.Img src={img} alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title>Card title</Card.Title>
+    <Card.Text>
+      Leaving this here in case we need text
+    </Card.Text>
+    
+  </Card.ImgOverlay>
+</Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ bucket-list/src/components/Bucket.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import BucketForm from './BucketForm';
+
+function Bucket(props) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: '',
+    eagerness: '',
+  });
+
+  console.log(props.bucket);
+
+  const submitUpdate = (value) => {
+    props.editBucketItem(edit.id, value);
+    setEdit({ id: null, value: '', eagerness: '' });
+  };
+
+  if (edit.id) {
+    return <BucketForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return props.bucket.map((item, i) => (
+    <div
+      className={
+        item.isComplete
+          ? `bucket-row complete ${item.eagerness}`
+          : `bucket-row ${item.eagerness}`
+      }
+      key={i}
+    >
+      <div key={item.id} onClick={() => props.completeBucketItem(item.id)}>
+        {item.text}
+      </div>
+      <div className="icons">
+        {console.log(item)}
+        <p onClick={() => setEdit({ id: item.id, value: item.text, eagerness: item.eagerness })}> ✏️</p>
+        <p onClick={() => props.removeBucketItem(item.id)}> 🗑️</p>
+      </div>
+    </div>
+  ));
+}
+
+export default Bucket;
