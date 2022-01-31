bd5541547835b5e40ee931594cb7d3b572a001c5
Jay <jbarbanel531@gmail.com>
2022-01-31 10:42:18 -0800

    testing

---
+++ bucket-list/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, 
+  Navbar, 
+  Container, 
+  Nav} from "react-bootstrap"
+
+import img from './images/signIn.png'
+// import img1 from './images/teamSelect.jpg'
+
+function App() {
+  return (
+    <div>
+
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+   <Card styles={{height:'300px'}}className="bg-dark text-white">
+  <Card.Img src={img} alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title>Card title</Card.Title>
+    <Card.Text>
+      Leaving this here in case we need text
+    </Card.Text>
+    <Card>
+  <Card.Body >This is some text within a card body.</Card.Body>
+</Card>
+  </Card.ImgOverlay>
+</Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ bucket-list/src/components/BucketForm.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+
+function BucketForm(props) {
+  const [input, setInput] = useState('');
+  let [eagerness, setEagerness] = useState('');
+
+  const eagernessLevel = ['high', 'medium', 'low']
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!eagerness) {
+      eagerness = 'low';
+    }
+
+    props.onSubmit({
+      id: Math.random(Math.floor() * 1000),
+      text: input,
+      eagerness: eagerness,
+    });
+
+    setInput('');
+    setEagerness('');
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  // First we check to see if "edit" prop exists. If not, we render the normal form
+  // If the prop "edit" exists, we know to render the update form instead
+  return !props.edit ? (
+    <div>
+      <form className="bucket-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add to your bucket list"
+          value={input}
+          name="text"
+          className="bucket-input"
+          onChange={handleChange}
+        ></input>
+        <div className="dropdown">
+          <button className={`dropbtn ${eagerness}`}>
+            {eagerness || 'Priority'}
+          </button>
+          <div className="dropdown-content">
+            <p onClick={() => setEagerness(eagernessLevel[0])}>Must do</p>
+            <p onClick={() => setEagerness(eagernessLevel[1])}>Want to do</p>
+            <p onClick={() => setEagerness(eagernessLevel[2])}>Take it or leave it</p>
+          </div>
+        </div>
+        <button className="bucket-button">Add bucket list item</button>
+      </form>
+    </div>
+  ) : (
+    <div>
+      <h3>Update entry: {props.edit.value}</h3>
+      <form className="bucket-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder={props.edit.value}
+          value={input}
+          name="text"
+          className="bucket-input"
+          onChange={handleChange}
+        ></input>
+        <div className="dropdown">
+          <button className={`dropbtn ${eagerness}`}>
+            {eagerness || 'Priority'}
+          </button>
+          <div className="dropdown-content">
+            <p onClick={() => setEagerness(eagernessLevel[0])}>Must do</p>
+            <p onClick={() => setEagerness(eagernessLevel[1])}>Want to do</p>
+            <p onClick={() => setEagerness(eagernessLevel[2])}>Take it or leave it</p>
+          </div>
+        </div>
+        <button className="bucket-button">Update</button>
+      </form>
+    </div>
+  );
+}
+
+export default BucketForm;
