eeac7718b76c11fe7dc86974fb914cfc8f486436
mediazjr <mediazjr@gmail.com>
2022-01-31 12:12:00 -0800

    added useState functionality to NBA.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,19 @@
 
-
+const Profile = require('./Profile');
 const Example = require('./Example');
 const User = require('./User');
 
-module.exports = {Example, User};
 
-const Profile = require('./Profile');
 
-module.exports = {Profile};
+
+
+
+
+
+
+module.exports = {Example, User, Profile};
+
+
+
+
 
