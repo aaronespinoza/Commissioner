e0f752e1d8df51ca15934ffa87291f1c0468d987
aaronespinoza <aarone@berkeley.edu>
2022-02-02 15:36:49 -0800

    test

--- client/src/pages/UpdatePage.js
+++ client/src/pages/UpdatePage.js
@@ -1,5 +1,5 @@
 
-import React from 'react';import React from "react";
+import React from 'react';
 // import "./UpdatePage.css";
 // import img3 from "../images/UpdateUser.png";
 //we import the mutations from the 
@@ -8,18 +8,19 @@ import { useMutation } from '@apollo/client';
 import { REMOVE_USER } from '../utils/mutations';
 import { UPDATE_TEAM } from '../utils/mutations';
 
-import React from 'react';
 // import "./UpdatePage.css";
 import img3 from "../images/UpdateUser.png";
 
 
 import {
-  
+  Form,
+  Button,
    } from "react-bootstrap";
 
 const UpdatePage = () => {
   const [deleteUser, { error }] = useMutation(REMOVE_USER);
-  const [changeTeam, { error }] = useMutation(UPDATE_TEAM);
+  const [changeTeam, { teamerror }] = useMutation(UPDATE_TEAM);
+  const [formState, setFormState] = useState({ email: '', password: '' });
 
   const handleChangeTeam = async (favoriteTeam) => {
   const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -37,7 +38,25 @@ const UpdatePage = () => {
     } catch (err) {
       console.error(err);
     }
-  
+// submit update team form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+    //   Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
   };
     return(
 
@@ -48,7 +67,39 @@ const UpdatePage = () => {
           height: "100vh",
         }}
         >
-        
+        <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <div className="card-body">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                {/* <Link to="/">back to the homepage.</Link> */}
+              </p>
+            ) : (
+              <Form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <Button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </Form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          </div>
     </div>
  
 
@@ -59,6 +110,3 @@ const UpdatePage = () => {
 
 
 export default UpdatePage;
-
-  
-
