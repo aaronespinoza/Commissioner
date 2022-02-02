cec80129f5f9a964583e7ee9c31af6cddeacd17a
aaronespinoza <aarone@berkeley.edu>
2022-02-02 13:21:14 -0800

    update page in progress

--- client/src/pages/UpdatePage.js
+++ client/src/pages/UpdatePage.js
@@ -1,12 +1,38 @@
 import React from 'react';import React from "react";
 import "./UpdatePage.css";
 import img3 from "../images/UpdateUser.png";
+//we import the mutations from the 
+//utils on the client end
+import { useMutation } from '@apollo/client';
+import { REMOVE_USER } from '../utils/mutations';
+import { UPDATE_TEAM } from '../utils/mutations';
 
 import {
   
    } from "react-bootstrap" 
 
-function UpdatePage(){
+const UpdatePage = () => {
+  const [deleteUser, { error }] = useMutation(REMOVE_USER);
+  const [changeTeam, { error }] = useMutation(UPDATE_TEAM);
+
+  const handleChangeTeam = async (favoriteTeam) => {
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const { data } = await changeTeam({
+        variables: { bookData: { ...bookToSave } },
+      });
+      console.log(savedBookIds);
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  
+  };
     return(
 
     <div className="pt-5 justify-content-center align-items-center d-flex w-100"
@@ -16,6 +42,7 @@ function UpdatePage(){
           height: "100vh",
         }}
         >
+        
     </div>
  
 
@@ -24,4 +51,4 @@ function UpdatePage(){
 
 
 
-   export default UpatePage;
+export default UpdatePage;
