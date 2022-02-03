f9c82905ec681bdb1f0fd656edd4529e69753e7b
Jay <jbarbanel531@gmail.com>
2022-02-03 14:31:14 -0800

    test

--- client/src/components/homeBg.js
+++ client/src/components/homeBg.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import homeImg from "../images/signIn.jpg";
+import React from "react";
+import mainImg from "../../images/signin.jpg";
 
-function HomeBg(){
-    return(
-<div
-    style={{
-        backgroundImage: `url(${homeImg})`,
-        backgroundSize: "cover",
-        height: "100vh",
-    }} >
-</div>
+function HomeBg (){
+    return (
+        <div className="pt-5 justify-content-center align-items-center d-flex w-100">
+            style={{
+                backgroundImage: `url(${mainImg})`,
+                backgroundSize: "cover",
+                height: "100vh"
+            }}
+        </div>
     )
 }
 
-export default HomeBg;
+export default HomeBg; 
